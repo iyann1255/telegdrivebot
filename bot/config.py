cd71b91c7febd1ce2d102f43032a4a0d86fc9347
iyann1255 <tessbarca1981@gmail.com>
2024-07-09 06:41:21 +0700

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,8 +3,8 @@ class config:
     APP_ID = "23765163"
     API_HASH = "008d5313d9006539373c2513373778cd"
     DATABASE_URL = "postgres://mdvgqgverqgesc:263a6b5b49f8539c4b1bb106ff7b300e5b5ebb3b46c3314fc57e8da9865c6457@ec2-34-192-122-0.compute-1.amazonaws.com:5432/d6c18273hu4a0r"
-    SUDO_USERS = " "  # Sepearted by space.
-    SUPPORT_CHAT_LINK = ""
+    SUDO_USERS = "6206131497"  # Sepearted by space.
+    SUPPORT_CHAT_LINK = "https://t.me/+rgO7YKnIM8ZjMjM1"
     DOWNLOAD_DIRECTORY = "./downloads/"
     G_DRIVE_CLIENT_ID = "966577441449-e0umm3642mq62ekq337s4hd2othm2bqd.apps.googleusercontent.com"
     G_DRIVE_CLIENT_SECRET = "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"
